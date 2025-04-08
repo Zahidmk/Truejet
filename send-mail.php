ffb84eeed9244479cb20db8c8c8108eddf7f9b7c
mkzahid <91457816+Zahidmk@users.noreply.github.com>
2025-04-08 21:47:38 +0530

    [updt]: mail update

--- send-mail.php
+++ send-mail.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = htmlspecialchars($_POST['phone']);
     $message = htmlspecialchars($_POST['message']);
 
-    $to = 'jestinvj4@gmail.com'; // your actual email
+    $to = 'mkzahid673@gmail.com'; // your actual email
     $subject = 'New Contact Form Submission';
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
